e1f41c4ea24962e0f6c7c22c746bbe2056835c1e
Saurav <sp.gharti@gmail.com>
2018-08-14 15:36:26 +0000

    add package django-shop

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-shop', 'url': 'http://pypi.python.org/pypi/django-shop/0.11.3'},
 	{'name':'django-oscar', 'url': 'http://pypi.python.org/pypi/django-oscar/1.5.1'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
 	{'name':'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
